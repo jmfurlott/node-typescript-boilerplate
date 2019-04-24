071303030e19e9c0ebc2d4304ef6d1a7eb3b867d
Joseph Furlott <jmfurlott@gmail.com>
2019-04-24 07:56:39 -0400

    ENH: Adds jest with typescript support

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,8 @@ app.get('/', function(req, res) {
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
+
+
+module.exports = {
+  testFn: () => "jest works",
+};
