df40477e367cc4cdd2dd805cf1f007c48a32e9ad
Joseph Furlott <jmfurlott@gmail.com>
2019-04-24 08:18:33 -0400

    BLD: Adds eslint-watch and start task

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import express = require("express");
 // Create a new express application instance
 const app: express.Application = express();
 
-app.get("/", function(req, res) {
+app.get("/", function(req, res): void {
   res.send("Hello World!");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function(): void {
   console.log("Example app listening on port 3000!");
 });
 
 module.exports = {
-  testFn: () => "jest works",
+  testFn: (): string => "jest works",
 };
