cd0dea3965b0abd12d6e2e4ab662eed0e8080a6d
Joseph Furlott <jmfurlott@gmail.com>
2019-04-24 08:10:53 -0400

    ENH: Adds prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   },
   extends:  [
     "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   rules:  {
     "@typescript-eslint/indent": ["warning", 2]
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import express = require('express');
+import express = require("express");
 
 // Create a new express application instance
 const app: express.Application = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get("/", function(req, res) {
+  res.send("Hello World!");
 });
 
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
-
 
 module.exports = {
   testFn: () => "jest works",
